2177b613fbec5d6fb2fe5b7cb69965df64732abb
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-04-16 16:19:46 -0600

    add rimshot

--- server.js
+++ server.js
@@ -111,6 +111,8 @@ app.get('/any-url', function(request, response){
     io.emit('applause', "Yay!");
   } else if (url === "cha-ching" || url === "payment" || url === "cha ching") {
     io.emit('chargeSucceeded', "Cha-ching!");
+  } else if (url === "rim-shot" || url === "rim shot" || url === "joke") {
+    io.emit('rimShot', "bad joke");
   } else if (url === "choo-choo" || url === "choo choo" || url === "train") {
     io.emit('trainDone', "choo choo");
   } else if (url === "crash") {
